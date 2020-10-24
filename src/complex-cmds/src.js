26d25ae9b3cec9958306f2f39ed9b6470372424f
sizzle-psr <sizzlerunspokemon@gmail.com>
2020-10-24 15:44:44 +0100

    Implement edge case handling for src and add video + subcategories support

--- src/complex-cmds/src.js
+++ src/complex-cmds/src.js
@@ -1,11 +1,22 @@
+const { concatLimit } = require('async');
 const fetch = require('node-fetch');
 const ret_codes = require('../utils/retcodes');
 
 function handler(separated, client, target) {
-    let game = separated[1];
+    let game = separated[1];   
+    separated.shift();
+    separated.shift();
+    let may_subcategory = separated.join(' ').split(' | ');
+    var subcategory;
+
+    if (may_subcategory.length > 1) { // Has subcategory
+        subcategory = may_subcategory[1];
+    }
+
+    separated = may_subcategory[0].split(' ');
+
     let place = separated[separated.length-1];
-    separated.shift();
-    separated.shift();
+
     separated.pop();
     let category = separated.join(' ');
     let url_game = 'https://www.speedrun.com/api/v1/games/' + game + '/categories';
@@ -14,29 +25,56 @@ function handler(separated, client, target) {
     const request = async () => {
         var res = await fetch('https://www.speedrun.com/api/v1/games/' + game);
         var res_json = await res.json();
-        var names = await res_json.data.names;
-        game_name = names.international;
+
+        if (res_json.status) {
+            client.say(target, 'Could not find the game ' + game + '.');
+            return;
+        }
+
+        game_name = await res_json.data.names.international;
     
         var cat_id;
         res = await fetch(url_game);
         res_json = await res.json();
 
         for (var cat in res_json.data) {
-            if (res_json.data[cat].name === category) {
+            if (res_json.data[cat].name.toLowerCase() === category.toLowerCase()) {
                 cat_id = res_json.data[cat].id;
                 break;
             }
         } 
 
-        // Check if we found it
+        if (!cat_id) {
+            client.say(target, category + ' is not a category of ' + game_name + '.');
+            return; 
+        }
+
         let url_variable = 'https://www.speedrun.com/api/v1/categories/' + cat_id + '/variables';
         
         var variables = [];
         res = await fetch(url_variable);
         res_json = await res.json();
-        for (var variable in res_json.data) {
-            // console.log(res_json.data[variable]);
-            variables[res_json.data[variable].id] = res_json.data[variable].values.default;
+
+        // !src_test pkmnfrlg Any% 1 | JPN
+        if (subcategory) {
+            // Take default value
+            for (var variable in res_json.data) {
+                for (var key in res_json.data[variable].values.values) {
+                    if (subcategory.toLowerCase() === res_json.data[variable].values.values[key].label.toLowerCase()) {
+                        variables[res_json.data[variable].id] = key;
+                        break;
+                    }
+                }
+            }
+        } else {
+            // Take default value
+            for (var variable in res_json.data) {
+                variables[res_json.data[variable].id] = res_json.data[variable].values.default;
+            }
+        }
+        if (Object.keys(variables).length < 1) {
+            client.say(target, subcategory + ' is not a valid subcategory of ' + game + ' ' + category + '.');
+            return;
         }
         
         let url = 'https://www.speedrun.com/api/v1/leaderboards/' + game + '/category/' + cat_id + '?';
@@ -57,7 +95,8 @@ function handler(separated, client, target) {
         res = await fetch(url);
         let r = await res.json();
         let data = r.data;
-        run = data.runs[Number(place)-1];
+
+        // Get the ordinal termination for the output message
         if (place.endsWith('13') || place.endsWith('12') || place.endsWith('11')) {
             termination = 'th';
         } else if (place.endsWith('3')) {
@@ -69,8 +108,18 @@ function handler(separated, client, target) {
         } else {
             termination = 'th';
         }
+
+        if (data.runs.length < Number(place)) {
+            client.say(target, game_name + ' ' + category + ' does not have a ' + place + termination + ' run. There are only ' + data.runs.length + ' runs.');
+            return;
+        }
+
+        run = data.runs[Number(place)-1];
+        let video_url = run.run.videos.links[0].uri;
         let user_id = run.run.players[0].id;
         let run_time_iso = run.run.times.primary;
+
+        // Convert ISO8601 to readable format
         run_time = run_time_iso.replace("PT","").replace("H",":").replace("M",":").replace("S","");
         run_time = run_time.split(':');
         let tmp = run_time[0];
@@ -80,11 +129,20 @@ function handler(separated, client, target) {
         }
         run_time = run_time.join(':');
         run_time = tmp + ':' + run_time;
+
+        // Get the user of the queried run
         res = await fetch('https://www.speedrun.com/api/v1/users/' + user_id);
         res_json = await res.json();
         data = await res_json.data;
         user_name = await data.names;
-        message = await place + termination + ' place in ' + game_name + ' ' + category + ' is ' + user_name.international + ' with a time of ' + run_time + '.';
+        if (!subcategory) {
+            message = await place + termination + ' place in ' + game_name + ' ' + category + 
+                     ' is ' + user_name.international + ' with a time of ' + run_time + '.';
+        } else {
+            message = await place + termination + ' place in ' + game_name + ' ' + category +
+                     ' ' + subcategory + ' is ' + user_name.international + ' with a time of ' + run_time + '.';
+        }
+        message = message + ' | Video: ' + video_url;
         client.say(target, message);
     }
     
